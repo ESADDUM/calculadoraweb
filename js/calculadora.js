d43594b601b993c7e83bb38246c2ced04b1322d8
ESADDUM <eduaddum@gmail.com>
2023-11-17 21:14:06 -0300

    ajuste arquivo java

--- js/calculadora.js
+++ js/calculadora.js
@@ -26,17 +26,17 @@ function voltar () {
 		throw new Error("Impossível Calcular");*/
 
 function calcular() {
-    var resultadoElemento = document.getElementById('resultado');
-    var resultado = resultadoElemento.innerHTML.trim();
+    var resultadoElement = document.getElementById('resultado');
+    var resultado = resultadoElement.innerHTML.trim();
     
     /* verifica se o ultimo caractere é um numero*/
     var ultimoCaractereNumerico = /\d$/. test(resultado);
     /*essa é uma expressão nativa do javascript: onde /\d/ é uma classe de caracteres que corresponde a qualquer dígito decimal (0-9) e $: Isso representa o final de uma string.*/ 
     
     if (ultimoCaractereNumerico) {
-        resultadoElement.innerHTML = eval(resultado).toFixed(2);
+        resultadoElement.innerHTML = eval(resultado).toFixed(2); /* eval faz o calculo na forma de uma expressão Ex: 1455+5-10 o resultado sera = 1450 tofixed(2) trava em 2 casas decimais*/
     } else {
-        alert("Mensagem de erro: Impossível Calcular");
+        alert("Mensagem de erro: Impossível Calcular"); /* a função alert chama uma pop up com a mensagem de erro*/
         limpar(); /* Chama a função limpar*/
     }
 }
